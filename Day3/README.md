acc19fc4cb490960a32bcbdde81fd05aec1150bd
Ankesh Singh <191995624+ankeshsingh00@users.noreply.github.com>
2026-03-30 11:17:56 +0000

    Day3: 4x1 MUX simulation and synthesis

---
+++ Day3/README.md
@@ -0,0 +1,22 @@
+# Day 3 - 4x1 MUX
+
+## Theory
+MUX = Multiplexer
+Selects one input from many inputs
+based on select lines
+
+## Truth Table
+| s1 | s0 | y  |
+|----|----|----|
+|  0 |  0 | i0 |
+|  0 |  1 | i1 |
+|  1 |  0 | i2 |
+|  1 |  1 | i3 |
+
+## Simulation
+iverilog mux_4x1.v tb_mux_4x1.v
+./a.out
+gtkwave mux_4x1.vcd
+
+## Synthesis
+Used sky130 PDK library
---
+++ Day3/tb_mux_4x1.v
@@ -0,0 +1,25 @@
+module tb_mux_4x1;
+
+  reg i0, i1, i2, i3;
+  reg s0, s1;
+  wire y;
+
+  mux_4x1 uut(
+    .i0(i0), .i1(i1),
+    .i2(i2), .i3(i3),
+    .s0(s0), .s1(s1),
+    .y(y)
+  );
+
+  initial begin
+    $dumpfile("mux_4x1.vcd");
+    $dumpvars(0, tb_mux_4x1);
+
+    i0=1; i1=0; i2=1; i3=0;
+    s1=0; s0=0; #10;
+    s1=0; s0=1; #10;
+    s1=1; s0=0; #10;
+    s1=1; s0=1; #10;
+  end
+
+endmodule
